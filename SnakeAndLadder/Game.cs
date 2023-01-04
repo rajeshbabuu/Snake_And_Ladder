2d6f951953e80bdc5a7339897f86a64d1adf6333
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-04 10:37:49 +0530

    Start game with a single player

---
+++ SnakeAndLadder/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Game
+    {
+        public void player1()
+        {
+            int Player1 = 1; 
+            int Position1 = 0;
+
+            Console.WriteLine("Starting first " + Player1 );
+            Console.WriteLine("player first position : " + Position1);
+        }
+
+    }
+}
---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------Welcome to the Snake and Ladder program");
+
+            Game snakegame = new Game();
+            snakegame.player1();
+        }
+    }
+}
