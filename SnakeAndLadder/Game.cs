5bc882fb8d98884f6e427bc69b59ae4fa0ce9cb9
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-04 22:25:46 +0530

    Player wins at Exact 100 Position

--- SnakeAndLadder/Game.cs
+++ SnakeAndLadder/Game.cs
@@ -9,8 +9,8 @@ namespace SnakeAndLadder
 {
     public class Game
     {
-        
-        
+
+
         public const int FORWARD_MOVE = 1;
         public const int BACKWARD_MOVE = 2;
         public const int NO_MOVE = 3;
@@ -20,39 +20,41 @@ namespace SnakeAndLadder
             int position = 0;
 
             Random random = new Random();
-            // Random random2 = new Random();
-
 
             while (position < 100)
             {
                 int movingCheck = random.Next(1, 3);
                 int stepCheck = random.Next(1, 6);
-                Console.WriteLine("this is moving check " + movingCheck);
-                Console.WriteLine("this is step check " + stepCheck);
+                Console.WriteLine(" this is moving check " + movingCheck);
+                Console.WriteLine(" this is step check " + stepCheck);
                 switch (movingCheck)
                 {
                     case FORWARD_MOVE:
                         if ((position + stepCheck) <= 100)
                         {
                             position = position + stepCheck;
-                            Console.WriteLine("Forward move");
+                            Console.WriteLine(" Forward move ");
                         }
                         break;
                     case BACKWARD_MOVE:
                         if (position > 0)
                         {
                             position = position - stepCheck;
-                            Console.WriteLine("Backward move");
+                            Console.WriteLine(" Backward move ");
                         }
                         break;
                     case NO_MOVE:
-                        Console.WriteLine("No move");
+                        Console.WriteLine("No move ");
                         break;
                 }
                 Console.WriteLine(position + " step count ");
 
 
+            }
 
+            if (position == 100)
+            {
+                Console.WriteLine("==========Player Wins==========");
             }
         }
     }
